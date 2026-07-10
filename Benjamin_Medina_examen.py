7d618a3734d591fee432a43dff8bc81220d9b731
JoJoFruna <jojofruna@gmail.com>
2026-07-10 09:49:13 -0400

    cambio 2

--- Benjamin_Medina_examen.py
+++ Benjamin_Medina_examen.py
@@ -38,3 +38,46 @@ def buscar_codigo(codigo, diccionario):
         if clave.upper() == codigo.upper():
             return True
     return False
+
+def unidades_categoria(categoria, productos, stock):
+    total_unidades = 0
+    cat_buscar = categoria.strip().lower()
+    
+    for codigo, datos in productos.items():
+        if datos[1].lower() == cat_buscar:
+            if codigo in stock:
+                total_unidades += stock[codigo][1]
+                
+    print(f"El total de unidades disponibles es: {total_unidades}")
+
+def busqueda_precio(p_min, p_max, productos, stock):
+    resultados = []
+
+    for codigo, datos_stock in stock.items():
+        precio = datos_stock[0]
+        unidades = datos_stock[1]
+    
+        if p_min <= precio <= p_max and unidades > 0:
+            if codigo in productos:
+                nombre = productos[codigo][0]
+                resultados.append(f"{nombre}--{codigo}")
+    
+    if resultados:
+        resultados.sort()  
+        print(f"Los productos encontrados son: {resultados}")
+    else:
+        print("No hay productos en ese rango de precios.")
+
+def actualizar_precio(codigo, nuevo_precio, stock):
+    clave_real = None
+    for k in stock.keys():
+        if k.upper() == codigo.upper():
+            clave_real = k 
+            break
+    
+    if clave_real is not None:
+        stock[clave_real][0] = nuevo_precio
+        return True
+    return False
+
+def
